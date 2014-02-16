4840e5ba4992d5f644ef3c0e7ca43ac0a79dcee4
Gustavo Muniz <gustavocms@hotmail.com>
2014-02-16 16:21:54 -0300

    Setting date but not getting on load

--- TopTal.ToDoList/TopTal.ToDoList/Scripts/app/ko.bindingHandlers.js
+++ TopTal.ToDoList/TopTal.ToDoList/Scripts/app/ko.bindingHandlers.js
@@ -2,15 +2,24 @@
 // Reusable bindings 
 
 ko.bindingHandlers.datepicker = {
-	init: function (element, valueAccessor) {
-		$(element).datepicker({
+    init: function (element, valueAccessor) {
+        $(element).datepicker({
 		    format: "dd/mm/yyyy",
 		    todayBtn: "linked",
 		    autoclose: true
-		});
-	},
-	update: function (element, valueAccessor) {
-	    var observable = valueAccessor();
-	    observable($(element).datepicker("getDate"));
-	}
+        }).on("changeDate", function (ev) {
+            var observable = valueAccessor();
+            observable(ev.date);
+        });
+    },
+    update: function (element, valueAccessor) {
+        var value = ko.utils.unwrapObservable(valueAccessor());
+
+        var widget = $(element).data("datepicker");
+        //when the view model is updated, update the widget
+        if (widget) {
+            widget.date = value;
+            widget.setValue();
+        }
+    }
 };
