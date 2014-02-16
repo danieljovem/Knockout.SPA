089c944241dddd4fd482004b72d18ff0dfcac59b
Gustavo Muniz <gustavocms@hotmail.com>
2014-02-16 19:18:10 -0300

    Calendar fixed

--- TopTal.ToDoList/TopTal.ToDoList/Scripts/app/ko.bindingHandlers.js
+++ TopTal.ToDoList/TopTal.ToDoList/Scripts/app/ko.bindingHandlers.js
@@ -4,22 +4,18 @@
 ko.bindingHandlers.datepicker = {
     init: function (element, valueAccessor) {
         $(element).datepicker({
-		    format: "dd/mm/yyyy",
-		    todayBtn: "linked",
+            format: "dd/mm/yyyy",
+            todayHighlight: true,
+            todayBtn: "linked",
+            clearBtn: true,
 		    autoclose: true
         }).on("changeDate", function (ev) {
             var observable = valueAccessor();
-            observable(ev.date);
+            observable(ev.date == null ? null : ev.date);
         });
     },
     update: function (element, valueAccessor) {
         var value = ko.utils.unwrapObservable(valueAccessor());
-
-        var widget = $(element).data("datepicker");
-        //when the view model is updated, update the widget
-        if (widget) {
-            widget.date = value;
-            widget.setValue();
-        }
+        $(element).datepicker("setDate", value == null ? null : new Date(value));
     }
 };
